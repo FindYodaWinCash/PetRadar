5de731b8d5cc29350ed362ddd62d38383b3f3123
FindYodaWinCash <dfelcan@gmail.com>
2021-02-11 16:25:09 -0500

    QueueQueries and Tests built

--- Tests/test_queueQueries.py
+++ Tests/test_queueQueries.py
@@ -1,5 +1,18 @@
 import pytest
+import boto3
+import time
 import Lambda.QueueQueries as qq
 
-def test_successful_execution():
-   return qq.queueQueries(False,False)
+TEST_JSON = {"test" : True}
+NUM_TEST_QUERIES = 2
+
+
+def test_retrieve_queries():
+   assert(qq.queueQueries(TEST_JSON,None) == NUM_TEST_QUERIES)
+
+def test_wrote_to_queue():
+   sqs = boto3.client('sqs')
+   time.sleep(5) #wait for it to appear on the queue.
+   response = sqs.get_queue_attributes(QueueUrl= qq.QUEUE_URL, AttributeNames=['ApproximateNumberOfMessages'])
+   assert int(response['Attributes']['ApproximateNumberOfMessages']) == NUM_TEST_QUERIES
+  
