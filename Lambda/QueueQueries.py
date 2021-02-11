04708d9695658af6c75dece08ca5f1ada8b489de
FindYodaWinCash <dfelcan@gmail.com>
2021-02-11 16:26:07 -0500

    conftest and queuequeries changes made

--- Lambda/QueueQueries.py
+++ Lambda/QueueQueries.py
@@ -1,3 +1,20 @@
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+QUERY_TABLE_NAME='PetRadar_Queries'
+QUEUE_URL = 'https://sqs.us-east-2.amazonaws.com/614141687890/PetRadar_Query_Queue'
+
 def queueQueries(event,context):
-    #TO DO read from dynamo DB table the queries
-    return False
+    table = boto3.resource('dynamodb').Table(QUERY_TABLE_NAME)
+    sqs = boto3.client('sqs')
+
+    response = None
+    if 'test' in event:
+        response = table.scan(FilterExpression=Attr('test').exists())
+    else:
+        response = table.scan(FilterExpression=Attr('test').not_exists())
+
+    for item in response['Items']:
+        sqs.send_message(QueueUrl=QUEUE_URL,MessageBody=str(item))
+
+    return response['Count']
